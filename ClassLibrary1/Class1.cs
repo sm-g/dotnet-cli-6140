116c65eb6f396fe102a42cbaa2b6c0ef628b1219
smg <semagerasimov@gmail.com>
2017-11-16 23:22:13 +0300

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary462
+{
+    public class Class1
+    {
+        public static void Test()
+        {
+            Console.WriteLine(new HttpClient());
+        }
+    }
+
+    public class X
+    {
+        [Required]
+        public bool Q { get; set; }
+    }
+}
---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//using System.ComponentModel.DataAnnotations;
+using System.Net.Http;
+
+namespace ClassLibraryNetStandart
+{
+    public class Class1
+    {
+        public static void Test()
+        {
+            Console.WriteLine(new HttpClient());
+        }
+    }
+}
